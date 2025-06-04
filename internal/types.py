4d39cd697dac5d2e2edc4206b0d111e01830ce0c
Rimu Eirnarn <Rimuru720@proton.me>
2025-06-04 23:51:46 +0700

    Add Colors type alias to types.py for improved color handling

--- internal/types.py
+++ internal/types.py
@@ -7,5 +7,6 @@ number: TypeAlias = int | float # pylint: disable=invalid-name
 AnchorEnum: TypeAlias = Literal['bottomleft', 'bottomright', 'topleft', 'topright']
 RGB: TypeAlias = tuple[int, int, int]
 RGBA: TypeAlias = tuple[int, int, int, int]
+Colors: TypeAlias = RGB | RGBA
 
 __all__ = ['number']
