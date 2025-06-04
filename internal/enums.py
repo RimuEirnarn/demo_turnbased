4f019ea98cf45a62d5c032cf8e6f07c74b96693d
Rimu Eirnarn <Rimuru720@proton.me>
2025-06-04 23:51:52 +0700

    Add Elements and EntityType enums to enums.py for improved type handling

--- internal/enums.py
+++ internal/enums.py
@@ -1,4 +1,5 @@
 """Enums"""
+# pylint: disable=invalid-name
 
 from enum import IntEnum
 
@@ -8,3 +9,21 @@ class StateEnum(IntEnum):
     NOT_ENOUGH_ENERGY = -2
     UNDEFINED = -999
     OK = -1000
+
+class Elements(IntEnum):
+    """Elements"""
+
+    Radiance = 1
+    Abyss = 2
+    Pyro = 3
+    Hydro = 4
+    Electro = 5
+    Glacio = 6
+    Aero = 7
+    Geo = 8
+    Plantea = 9
+
+class EntityType(IntEnum):
+    """Entity Type"""
+    CHARACTERS = 1
+    ENEMY = 0
