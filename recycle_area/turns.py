6ac419ee0692c98d48e183fe79f3ca28690c2f36
Rimu Eirnarn <Rimuru720@proton.me>
2025-06-05 03:45:05 +0700

    Update Hero actor's speed to 400 and adjust enemy speeds for balance

--- recycle_area/turns.py
+++ recycle_area/turns.py
@@ -1,3 +1,4 @@
+# pylint: disable=missing-function-docstring,missing-module-docstring,wrong-import-position,invalid-name
 from math import ceil
 import sys
 import os.path
@@ -5,26 +6,28 @@ import pygame
 
 import tabulate
 
+
 sys.path.append(os.path.abspath("./"))
 
-from internal.turn_system import ActionQueue, base_av, ENEMY_BASE_SPD
+from internal.types import number
+from internal.turn_system import Action, ActionQueue, advance, base_av, ENEMY_BASE_SPD
 
-pygame.quit() # pylint: disable=no-member
+pygame.quit()  # pylint: disable=no-member
 
 # Example actors
 
 actors = [
-    {"type": "enemy", "name": "Goblin 1", "spd": ENEMY_BASE_SPD},
-    {"type": "character", "name": "Hero", "spd": 800},
+    {"type": "enemy", "name": "Goblin 1", "spd": ENEMY_BASE_SPD * 2},
+    {"type": "character", "name": "Hero", "spd": 400},
     {"type": "summon", "name": "Fire Sprite", "spd": 230},
     {"type": "character", "name": "Healer", "spd": 200},
     {"type": "character", "name": "Support slow", "spd": 95},
     {"type": "character", "name": "Support FAST", "spd": 180},
     {"type": "character", "name": "Sub-DPS", "spd": 340},
-    {"type": "enemy", "name": "Goblin 2", "spd": ENEMY_BASE_SPD},
-    {"type": "enemy", "name": "Goblin 3", "spd": ENEMY_BASE_SPD},
-    {"type": "enemy", "name": "Goblin 4", "spd": ENEMY_BASE_SPD},
-    {"type": "enemy", "name": "Goblin 5", "spd": ENEMY_BASE_SPD},
+    {"type": "enemy", "name": "Goblin 2", "spd": ENEMY_BASE_SPD * 2},
+    {"type": "enemy", "name": "Goblin 3", "spd": ENEMY_BASE_SPD * 2},
+    {"type": "enemy", "name": "Goblin 4", "spd": ENEMY_BASE_SPD * 2},
+    {"type": "enemy", "name": "Goblin 5", "spd": ENEMY_BASE_SPD * 2},
 ]
 
 # Create the queue
@@ -40,23 +43,61 @@ def safe_input():
     return "0" if not r else r[0]
 
 
+hero = None
+enemy = None
+sub_dps = None
+adv = 0.7
+loop = 0
+
+
+def advg(acting_actor: Action, adv_value: number = 0, adv_delay: number = 0):
+    new_av = advance(acting_actor.value, acting_actor.base_value, adv_value, adv_delay)
+    prev_av = acting_actor.value
+    adv_index = q.predict_action_index_after_update(acting_actor.id, new_av)
+    print(
+        f"Advancing {acting_actor.source['name']} by {(adv_value-adv_delay)*100:.0f}%, from {ceil(prev_av)} -> {ceil(new_av)}, positioning in {adv_index} ticks"
+    )
+    q.update_action_value(acting_actor.id, new_av)
+
+
 # Process turns
 while True:
-    next_act = q.pop_reinsert()
+    subdps = False
+    next_act: Action = q.pop_reinsert()
+    if next_act.source["name"] == "Hero":
+        hero = next_act
+    if next_act.source["name"] == "Sub-DPS":
+        subdps = True
+        sub_dps = next_act
+    if next_act.source["name"] == "Goblin 1":
+        enemy = next_act
     next_index = q.predict_next_turn_index(next_act.id)
-    msg = "Will act immediately" if next_index == 0 else f"Will act in {next_index+1} ticks"
-    print(
-        f"\n{next_act.source['name']} acts. {msg}"
+    msg = (
+        "Will act immediately"
+        if next_index == 0
+        else f"Will act in {next_index+1} ticks"
     )
+    print(f"\n{next_act.source['name']} acts. {msg}")
     print(
         tabulate.tabulate(
             (
                 (index + 1, act.source["name"], ceil(act.value), round(act.value, 4))
                 for index, act in enumerate(q)
             ),
-            headers=("Ticks", "Name", "AV", "Real AV"),
+            headers=("Ticks", "Name", "AV"),
             tablefmt="simple_outline",
         )
     )
+    if subdps:
+        hero = q.get_action(hero.id)
+        sub_dps = q.get_action(sub_dps.id)
+        advg(hero, adv)
+        advg(sub_dps, 0.5)
+        if enemy:
+            enemy = q.get_action(enemy.id)
+            advg(enemy, 0, 2)
+
     if safe_input()[0] == "q":
         break
+
+    loop += 1
