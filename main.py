93bbee187125665726a0e7958a7963e36f8b88fb
Rimu Eirnarn <Rimuru720@proton.me>
2025-06-06 04:14:47 +0700

    Adjust enemy shield scaling for improved defensive mechanics

--- main.py
+++ main.py
@@ -126,11 +126,11 @@ def update_main():
         defender = enemy
 
     if enemy.hp <= (enemy.max_hp * 0.5) and enemy_shield_time is False:
-        enemy.shield += enemy.max_hp * 12.75
+        enemy.shield += enemy.max_hp * 15
         enemy_shield_time = True
 
     if enemy.hp <= (enemy.max_hp * 0.25) and enemy_shield_time is True:
-        enemy.shield += 18.25 * enemy.max_hp
+        enemy.shield += 20 * enemy.max_hp
         enemy_shield_time = None
 
     draw_bars([player, enemy], attacker)
