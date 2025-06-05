62aa2c5be9146e511953a22557d5e0a2553e1089
Rimu Eirnarn <Rimuru720@proton.me>
2025-06-06 01:04:27 +0700

    Refactor player and enemy initialization; improve game loop structure and logging

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# pylint: disable=import-error,no-member,invalid-name,missing-class-docstring,missing-function-docstring,missing-module-docstring,unused-import
+# pylint: disable=import-error,no-member,invalid-name,missing-class-docstring,missing-function-docstring,missing-module-docstring,unused-import,global-statement,line-too-long
 import sys
 from math import ceil
 import random
@@ -11,17 +11,15 @@ from internal.gui.stats import draw_bars
 from internal.turn_system import ActionQueue, base_av
 
 # Demo characters
-player = Hero("Hero", hp=14500, atk=1135, defense=490, spd=800, mp=100)
+player = Hero("Hero", hp=14500, atk=1135, defense=200, spd=180, mp=100)
 player.shield += 7500
-enemy = Enemy("Dummy", hp=130_299_791_000, atk=500, defense=0, spd=20)
+enemy = Enemy("Dummy", hp=299_791_000, atk=500, defense=0, spd=140)
 null = Entity("<NULL>", 0, 0, 0, 0)
 
 # Turn state
 action_order = ActionQueue()
 action_order.add_action_by_value(player, base_av(player.spd))
 action_order.add_action_by_value(enemy, base_av(enemy.spd))
-turn_order = sorted([player, enemy], key=lambda x: x.spd, reverse=True)
-turn_index = 0
 
 enemy_shield_time = False
 
@@ -31,39 +29,36 @@ combat_log = ""
 skill_set = [1, 1, 2, 1, 1, 2]
 attack_index = 0
 
-player.generic_regen('energy', 1)
+player.generic_regen("energy", 1)
+screen.fill((50, 50, 50))
+log_action(
+    f"Cycle {action_order.cycles} | Actions {action_order.total_actions} | Total AVs {action_order.total_av} | [Q] to close",
+    (50, 22),
+)
 draw_bars([player, enemy])
 pygame.display.flip()
 
-while running:
-    screen.fill((50, 50, 50))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                running = False
-                continue
+halting = False
 
+
+def update_main():
+    global enemy_shield_time
     attacker_action = action_order.pop_next_action()
     attacker = attacker_action.source
     defender = "<NULL>"
-    skill_name = null
     this_dmg = 0
 
     if not player.is_alive() or not enemy.is_alive():
         winner = player.name if player.is_alive() else enemy.name
         print(f"Game Over. {winner} wins!")
         pygame.time.wait(2000)
-        running = False
-        continue
+        return True
 
     if isinstance(attacker, Enemy):
         pygame.time.wait(1000)
         defender = player
         this_dmg = attacker.attack(player)
-        player.generic_regen('energy', raw=15)
-        skill_name = "Enemy: Basic Attack"
+        player.generic_regen("energy", raw=15)
     if isinstance(attacker, Character):
         pygame.time.wait(750 + random.randint(0, 500))
         if attacker.energy == attacker.ult_encost:
@@ -84,8 +79,33 @@ while running:
         (50, 5),
     )
 
-    turn_index += 1
-    action_order.add_action_by_value(attacker, attacker_action.base_value, attacker_action.id)
+    action_order.add_action_by_value(
+        attacker, attacker_action.base_value, attacker_action.id
+    )
+    return False
+
+
+while running:
+    screen.fill((50, 50, 50))
+    log_action(
+        f"Cycle {action_order.cycles} | Actions {action_order.total_actions} | Total AVs {action_order.total_av} | [Q] to close",
+        (50, 22),
+    )
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                running = False
+                continue
+
+    if not halting:
+        halting = update_main()
+    else:
+        draw_bars([player, enemy])
+        log_action("Game ends", (50, 5))
+
     clock.tick(60)
     pygame.display.flip()
 
