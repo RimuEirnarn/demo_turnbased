e62fb1299a9b73d56c138f31aa3640837f0873b6
Rimu Eirnarn <Rimuru720@proton.me>
2025-06-05 21:02:47 +0700

    Refactor game loop: update screen fill color, improve action handling, and streamline attacker/defender logic

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import pygame
 
 from internal.basic_graphics import screen, BLACK, log_action, clock
 from internal.characters.hero import Hero
-from internal.entities import Enemy, Character
+from internal.entities import Enemy, Character, Entity
 from internal.gui.stats import draw_bars
 from internal.turn_system import ActionQueue, base_av
 
 # Demo characters
 player = Hero("Hero", hp=3210, atk=2135, defense=490, spd=200, mp=100)
+player.shield += 7500
 enemy = Enemy("Dummy", hp=13_299_791_000, atk=500, defense=0, spd=20)
+null = Entity("<NULL>", 0, 0, 0, 0)
 
 # Turn state
 action_order = ActionQueue()
@@ -28,19 +30,24 @@ skill_set = [1, 1, 2, 1, 1, 2]
 attack_index = 0
 
 while running:
-    screen.fill(BLACK)
+    screen.fill((50, 50, 50))
     draw_bars([player, enemy])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                running = False
+                continue
 
-    attacker = turn_order[turn_index % 2]
-    defender = turn_order[(turn_index + 1) % 2]
-    skill_name = "Null"
+    attacker_action = action_order.pop_next_action()
+    attacker = attacker_action.source
+    defender = "<NULL>"
+    skill_name = null
     this_dmg = 0
 
-    if not attacker.is_alive() or not defender.is_alive():
-        winner = attacker.name if attacker.is_alive() else defender.name
+    if not player.is_alive() or not enemy.is_alive():
+        winner = player.name if player.is_alive() else enemy.name
         print(f"Game Over. {winner} wins!")
         pygame.time.wait(2000)
         running = False
@@ -48,22 +55,25 @@ while running:
 
     if isinstance(attacker, Enemy):
         pygame.time.wait(1000)
-        this_dmg = attacker.attack(defender)
+        defender = player
+        this_dmg = attacker.attack(player)
         skill_name = "Enemy: Basic Attack"
     if isinstance(attacker, Character):
         pygame.time.wait(750 + random.randint(0, 500))
         if attacker.energy == attacker.ult_encost:
-            this_dmg = attacker.ultimate(defender)
+            this_dmg = attacker.ultimate(enemy)
         elif attacker.mp >= attacker.skill_mpcost:
-            this_dmg = attacker.skill(target=defender)
+            this_dmg = attacker.skill(target=enemy)
         else:
-            this_dmg = attacker.basic_attack(defender)
+            this_dmg = attacker.basic_attack(enemy)
+        defender = enemy
     log_action(
         f"{attacker.name} attacks {defender.name} for {round(this_dmg):,} damage",
         (50, 5),
     )
 
     turn_index += 1
+    action_order.add_action(attacker, attacker_action.base_value, attacker_action.id)
     clock.tick(60)
     pygame.display.flip()
 
