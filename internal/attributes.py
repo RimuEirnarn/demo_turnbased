95f0db22b3150df7ad3a02286c6ffd34b9c6f9f0
Rimu Eirnarn <Rimuru720@proton.me>
2025-06-07 16:11:38 +0700

    Add Attributes and Elements classes for game mechanics

---
+++ internal/attributes.py
@@ -0,0 +1,71 @@
+"""Attributes"""
+
+from dataclasses import dataclass, field
+from math import ceil
+from typing import TypeVar
+
+from internal.elements import Elements
+from internal.types import number
+# pylint: disable=all
+
+T = TypeVar('Tnumber')
+
+DEFAULT_MAPPING = {
+    Elements.RADIANCE: 0,
+    Elements.ABYSS: 0,
+    Elements.PYRO: 0,
+    Elements.HYDRO: 0,
+    Elements.AERO: 0,
+    Elements.ELECTRO: 0,
+    Elements.GLACIO: 0,
+    Elements.GEO: 0,
+    Elements.PLANTEA: 0
+}
+
+@dataclass
+class Stat:
+    base: float = 0
+    multiplier: float = 0
+    additive: int = 0
+
+    @property
+    def value(self):
+        """Calculated value"""
+        return self.base * (1 + self.multiplier) + self.additive
+
+    def __int__(self):
+        return ceil(self.value)
+
+    def __float__(self):
+        return self.value
+
+    def __repr__(self):
+        return f"<Stat base={self.base} mult={1+self.multiplier}% add={self.additive}>"
+
+    def __add__(self, other: T) -> T:
+        return float(self) + other
+
+    def __sub__(self, other: T) -> T:
+        return float(self) - other
+
+    def __mul__(self, other: T) -> T:
+        return float(self) * other
+
+    def __div__(self, other: T) -> T:
+        return float(self) / other
+
+@dataclass
+class Attributes:
+    hp: Stat
+    atk: Stat
+    def_: Stat
+    spd: Stat
+
+    crit_rate: number = 0.05
+    crit_dmg: number = 0.5
+    effect_rate: number = 0
+    effect_res: number = 0
+
+    res: dict[Elements, number] = field(default_factory=DEFAULT_MAPPING.copy)
+    res_pen: dict[Elements, number] = field(default_factory=DEFAULT_MAPPING.copy())
+    bonus: dict[Elements, number] = field(default_factory=DEFAULT_MAPPING.copy())
---
+++ internal/elements.py
@@ -0,0 +1,15 @@
+"""Elements"""
+
+from enum import IntEnum, auto
+
+class Elements(IntEnum):
+    """Elements"""
+    RADIANCE = auto()
+    ABYSS = auto()
+    PYRO = auto()
+    HYDRO = auto()
+    ELECTRO = auto()
+    GLACIO = auto()
+    AERO = auto()
+    GEO = auto()
+    PLANTEA = auto()
