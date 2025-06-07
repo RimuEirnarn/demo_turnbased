c94992d30fc94e0c1d2985c0fc8712b9029e5a06
Rimu Eirnarn <Rimuru720@proton.me>
2025-06-07 20:56:42 +0700

    Add Coords and Size type aliases and update __all__ in types.py

--- internal/types.py
+++ internal/types.py
@@ -5,8 +5,10 @@ from typing import Literal, TypeAlias
 
 number: TypeAlias = int | float # pylint: disable=invalid-name
 AnchorEnum: TypeAlias = Literal['bottomleft', 'bottomright', 'topleft', 'topright']
+Coords: TypeAlias = tuple[int, int]
+Size: TypeAlias = tuple[int, int]
 RGB: TypeAlias = tuple[int, int, int]
 RGBA: TypeAlias = tuple[int, int, int, int]
 Colors: TypeAlias = RGB | RGBA
 
-__all__ = ['number']
+__all__ = ['number', 'AnchorEnum', 'Coords', "RGB", "RGBA", "Colors"]
